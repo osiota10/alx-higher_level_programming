1110582bc702a1596892711d14519ab400773d65
Osiota Samuel Obrozie <73504914+osiota10@users.noreply.github.com>
2022-12-01 16:17:16 +0100

    Update 100-my_calculator.py

--- 0x02-python-import_modules/100-my_calculator.py
+++ 0x02-python-import_modules/100-my_calculator.py
@@ -4,7 +4,6 @@ if __name__ == "__main__":
     import sys
     
     nargs = len(sys.argv) - 1
-
     if nargs != 3:
         print("Usage: ./100-my_calculator.py <a> <operator> <b>")
         sys.exit(1)
