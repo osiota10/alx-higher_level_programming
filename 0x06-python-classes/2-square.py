50d515fc85aba31e400d1122a32635dc4473aa73
Osiota Samuel Obrozie <73504914+osiota10@users.noreply.github.com>
2022-12-20 13:30:41 +0100

    Update 2-square.py

--- 0x06-python-classes/2-square.py
+++ 0x06-python-classes/2-square.py
@@ -2,6 +2,7 @@
 
 """Define a class Square."""
 
+
 class Square:
     """Represent a square."""
 
@@ -14,4 +15,4 @@ class Square:
             raise TypeError("size must be an integer")
         elif size < 0:
             raise ValueError("size must be >= 0")
-        self.__size = size
+        self.__size = 
